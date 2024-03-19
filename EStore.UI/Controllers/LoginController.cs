df62b8b2d6c27adbe479476414fc46aaf7418e7e
anilklc <hage4950@gmail.com>
2024-03-19 23:08:59 +0300

    Added pages for Policies, Terms and Conditions, Login, Register, and Contact

---
+++ EStore.UI/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EStore.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
