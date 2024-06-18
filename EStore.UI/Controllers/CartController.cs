05ddf8a4c054da05fb9b01c15301fb4f05bee28c
anilklc <hage4950@gmail.com>
2024-06-19 02:19:00 +0300

    Cart Started

--- EStore.UI/Controllers/CartController.cs
+++ EStore.UI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace EStore.UI.Controllers
 {
@@ -8,5 +9,23 @@ namespace EStore.UI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Checkout([FromBody] List<CartItem> cart)
+        {
+            // Sipariş işleme kodu buraya gelecek
+            // Örneğin: sipariş veritabanına kaydedilecek
+
+            // İşlemin başarılı olduğunu varsayıyoruz
+            return Json(new { success = true });
+        }
+    }
+
+    public class CartItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
