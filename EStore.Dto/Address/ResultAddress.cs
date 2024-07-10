6e2444ea2b619a612a6570c1ba7cf93c900361e0
anilklc <hage4950@gmail.com>
2024-07-10 22:24:15 +0300

    User address added

---
+++ EStore.Dto/Address/ResultAddress.cs
@@ -0,0 +1,15 @@
+﻿namespace EStore.Dto.Address
+{
+    public class ResultAddress
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string AddressTitle { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Detail { get; set; }
+    }
+}
--- EStore.UI/Areas/Account/Controllers/UserAddressController.cs
+++ EStore.UI/Areas/Account/Controllers/UserAddressController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EStore.Dto.Address;
+using EStore.Dto.Address;
+using EStore.Services.Helper;
+using EStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EStore.UI.Areas.Account.Controllers
 {
@@ -6,10 +10,55 @@ namespace EStore.UI.Areas.Account.Controllers
     [Route("Account/UserAddress")]
     public class UserAddressController : Controller
     {
+        private readonly IWriteService<CreateAddress, UpdateAddress> _writeService;
+        private readonly IReadService<ResultAddress> _readService;
+
+        public UserAddressController(IWriteService<CreateAddress, UpdateAddress> writeService, IReadService<ResultAddress> readService)
+        {
+            _writeService = writeService;
+            _readService = readService;
+        }
+
         [HttpGet("Index")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var addresss = await _readService.GetAll($"Addresses/GetAddressByUsername/{UserHelper.GetUserName(HttpContext)}", "address");
+            return View(addresss);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult CreateAddress()
         {
             return View();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CreateAddress(CreateAddress createAddress)
+        {
+            createAddress.UserName = UserHelper.GetUserName(HttpContext);
+            var result = await _writeService.AddAsync(createAddress, "Addresses/CreateAddress");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> RemoveAddress(string id)
+        {
+            await _writeService.DeleteAsync(id, "Addresses/RemoveAddress/");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> UpdateAddress(string id)
+        {
+            var address = await _readService.Get("Addresses/GetByIdAddress/", id);
+            return View(address);
+        }
+
+        [HttpPost("[action]/{id}")]
+        public async Task<IActionResult> UpdateAddress(UpdateAddress updateAddress)
+        {
+            var result = await _writeService.UpdateAsync(updateAddress, "Addresses/UpdateAddress/");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
